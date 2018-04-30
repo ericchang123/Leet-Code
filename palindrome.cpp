fb8749bdea5949a3b31c911542a019d6a96034fe
Eric Chang <echan036@ucr.edu>
2018-04-29 23:58:55 -0700

    First Commit

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            if(isalnum(s[i]) && isalnum(s[j])){
+                if(tolower(s[i]) != tolower(s[j]))
+                    return false;
+                else
+                    i++; j--;
+            }
+            else if(!isalnum(s[i]))
+                i++;
+            else if(!isalnum(s[j]))
+                j--;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
